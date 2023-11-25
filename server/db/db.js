4cf83e27afa164320ea44d33f98da0a41fa7d979
siddhant-sharma2033427 <ss2033427@gmail.com>
2023-11-25 23:18:31 +0530

    deff

--- server/db/db.js
+++ server/db/db.js
@@ -1,9 +1,9 @@
 import mongoose from 'mongoose';
-import dotenv from 'dotenv';
+// import dotenv from 'dotenv';
 
-dotenv.config();
-const DB_USER = process.env.DB_USER
-const DB_PASS = process.env.DB_PASS
+// dotenv.config();
+const DB_USER = "user"
+const DB_PASS = "User" 
 
 const URL = `mongodb+srv://${DB_USER}:${DB_PASS}@cluster0.feh5uzg.mongodb.net/?retryWrites=true&w=majority`
 const Connection = async () => {
