6479c3b29178084963b10e92aa3d2e5943a1bd52
siddhant-sharma2033427 <ss2033427@gmail.com>
2023-11-26 15:58:40 +0530

    ignore again conversation

--- client/src/components/chat/chat/Messages.jsx
+++ client/src/components/chat/chat/Messages.jsx
@@ -6,6 +6,7 @@ import { useContext } from 'react';
 import {newMessage,getMessage } from '../../../service/api'
 import Message from '../chat/Message'
 // import socket from 'socket.io'
+// import { io } from 'socket.io-client';
 const Wrapper = styled(Box)`
     background-image: url(${'https://user-images.githubusercontent.com/15075759/28719144-86dc0f70-73b1-11e7-911d-60d70fcded21.png'});
     background-size: 50%;
